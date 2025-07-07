8540a231b45d7b6e7fd7eda4a846fcc8a720c036
safayathussain <safayatofficial1@gmail.com>
2025-07-07 23:15:52 +0600

    cart model added

--- carts/admin.py
+++ carts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Cart, CartItem
 
-# Register your models here.
+class CartItemInline(admin.TabularInline):  # or StackedInline
+    model = CartItem
+    extra = 1  # how many empty image slots to show
+
+class CartAdmin(admin.ModelAdmin):
+    inlines = [CartItemInline]
+
+admin.site.register(Cart, CartAdmin)
--- carts/models.py
+++ carts/models.py
@@ -1,3 +1,25 @@
 from django.db import models
-
+from auths.models import CustomUser
+from products.models import Product
+from django.shortcuts import get_list_or_404
 # Create your models here.
+class Cart(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return f"Cart of {self.user.email}"
+    @property
+    def price(self): 
+        total_price = sum(item.price for item in self.cartitem_set.all())
+        return total_price
+
+    
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product= models.ForeignKey(Product, on_delete=models.CASCADE)
+    qty = models.PositiveIntegerField(default=1)
+    def __str__(self):
+        return f"{self.qty} of {self.product.name}"
+    @property
+    def price(self):
+        return self.product.price * self.qty
